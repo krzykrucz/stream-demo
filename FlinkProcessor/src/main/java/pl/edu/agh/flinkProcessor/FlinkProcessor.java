afc4e2d47df86ef1035e0f80903a4b032d8a46c4
Wojciech Karpiel <wkarpiel@student.agh.edu.pl>
2018-05-08 16:30:23 +0200

    minor fix

--- FlinkProcessor/src/main/java/pl/edu/agh/flinkProcessor/FlinkProcessor.java
+++ FlinkProcessor/src/main/java/pl/edu/agh/flinkProcessor/FlinkProcessor.java
@@ -1,9 +1,9 @@
-package flinkQ;
+package pl.edu.agh.flinkProcessor;
 
 
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer010;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer011;
 
 import java.util.Date;
@@ -19,7 +19,7 @@ public class FlinkProcessor {
         properties.setProperty("bootstrap.servers", "localhost:9092");
         properties.setProperty("zookeeper.connect", "localhost:2181");
         properties.setProperty("group.id", "my-topic");
-        env.addSource(new FlinkKafkaConsumer010<String>("my-source", new SimpleStringSchema(), properties))
+        env.addSource(new FlinkKafkaConsumer011<String>("my-source", new SimpleStringSchema(), properties))
                 .map(t -> "==[[" + t + "]]== " + new Date())
                 .addSink(new FlinkKafkaProducer011<String>(
                         "localhost:9092",      // Kafka broker host:port
