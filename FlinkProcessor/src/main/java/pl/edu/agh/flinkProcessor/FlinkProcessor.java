4523615608aca6789f717dfdee19349e7dde0bb7
Wojciech Karpiel <wkarpiel@student.agh.edu.pl>
2018-05-08 16:13:32 +0200

    pisanie + czytanie do i z kafki

--- FlinkProcessor/src/main/java/pl/edu/agh/flinkProcessor/FlinkProcessor.java
+++ FlinkProcessor/src/main/java/pl/edu/agh/flinkProcessor/FlinkProcessor.java
@@ -1,25 +1,14 @@
 package flinkQ;
 
 
-import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
-import org.apache.flink.api.java.tuple.Tuple2;
-import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer010;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
-import org.apache.flink.util.Collector;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer011;
 
+import java.util.Date;
 import java.util.Properties;
 
-/**
- * To run the example program, start the input stream with netcat first from a terminal:
- * <p>
- * nc -lk 9999
- * <p>
- * Just type some words hitting return for a new word. These will be the input to the word count program. If you want to see counts greater than 1, type the same word again and again within 5 seconds (increase the window size from 5 seconds if you cannot type that fast ☺).
- */
 public class FlinkProcessor {
 
     public static void main(String[] args) throws Exception {
@@ -30,27 +19,14 @@ public class FlinkProcessor {
         properties.setProperty("bootstrap.servers", "localhost:9092");
         properties.setProperty("zookeeper.connect", "localhost:2181");
         properties.setProperty("group.id", "my-topic");
-        DataStream<Tuple2<String, Integer>> dataStream = env
-                .addSource(new FlinkKafkaConsumer010<String>("my-source", new SimpleStringSchema(), properties) {
-                })
-                .flatMap(new Splitter())
-                .keyBy(0)
-                .timeWindow(Time.seconds(5))
-                .sum(1);
+        env.addSource(new FlinkKafkaConsumer010<String>("my-source", new SimpleStringSchema(), properties))
+                .map(t -> "==[[" + t + "]]== " + new Date())
+                .addSink(new FlinkKafkaProducer011<String>(
+                        "localhost:9092",      // Kafka broker host:port
+                        "my-proc",       // Topic to write to
+                        new SimpleStringSchema()));
 
-        dataStream.print();
-
-        env.execute("Window WordCount");
+        env.execute("xD");
     }
-
-    public static class Splitter implements FlatMapFunction<String, Tuple2<String, Integer>> {
-        @Override
-        public void flatMap(String sentence, Collector<Tuple2<String, Integer>> out) throws Exception {
-            for (String word : sentence.split(" ")) {
-                out.collect(new Tuple2<String, Integer>(word, 1));
-            }
-        }
-    }
-
 }
 
